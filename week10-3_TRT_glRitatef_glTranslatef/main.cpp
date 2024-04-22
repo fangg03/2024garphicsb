daf09fa526aa929140982a02fef17312b61f8f4a
fangg03 <112918847+fangg03@users.noreply.github.com>
2024-04-22 16:44:49 +0800

    week10

---
+++ week10-3_TRT_glRitatef_glTranslatef/main.cpp
@@ -0,0 +1,45 @@
+#include <GL/glut.h> ///使用 GLUT外掛 18行
+void myBody()
+{
+    glColor3f(1,0,0);
+    glutWireCube( 0.6 );
+}
+void myArm()
+{
+    glColor3f(0,1,0);
+        glScalef(1.5,0.5,0.5);
+        glutWireCube( 0.3 );
+}
+float angle=0; ///week10-2
+void display() ///自己寫
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+        ///glTranslatef(-0.6, +0.3,0);
+        myBody();
+    glPopMatrix();
+
+    glPushMatrix();
+        ///glTranslatef(+0.0,+0.3,0);
+        glRotatef(angle++, 0, 0, 1); ///week10-2
+        glTranslatef(0.255,0,0);
+        myArm();
+    glPopMatrix();
+
+    glutSwapBuffers(); ///自己寫
+}
+
+
+int main(int argc, char *argv[]) ///我們的 main函式 138行
+{
+    glutInit(&argc, argv); ///初始化 GLUT 140行
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); ///143行
+
+    glutCreateWindow("week10-3"); ///145行 建視窗
+
+    glutDisplayFunc(display); ///148行 設定要畫圖的display函式
+    glutIdleFunc(display); ///week10-2
+
+    glutMainLoop(); ///174行 主要迴圈
+}
---
+++ week10-5_TRT_two_layers/main.cpp
@@ -0,0 +1,53 @@
+#include <GL/glut.h> ///使用 GLUT外掛 18行
+void myBody()
+{
+    glColor3f(1,0,0);
+    glutWireCube( 0.6 );
+}
+void myArm()
+{
+    glPushMatrix();
+        glColor3f(0,1,0);
+        glScalef(1.5,0.5,0.5);
+        glutWireCube( 0.3 );
+    glPopMatrix();
+}
+float angle=0; ///week10-2
+void display() ///自己寫
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+        myBody();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(+0.3, +0.3,0); ///掛到肩上
+        glRotatef(angle++, 0, 0, 1); ///轉動
+        glTranslatef(0.255,0,0); ///旋轉中心，放在正中心
+        myArm(); ///右手臂
+
+        glPushMatrix(); ///week10-5
+            glTranslatef(0.255, 0,0); ///掛到肩上
+            glRotatef(angle++, 0, 0, 1); ///轉動
+            glTranslatef(0.255,0,0); ///旋轉中心，放在正中心
+            myArm();
+        glPopMatrix(); ///week10-5
+    glPopMatrix();
+
+    glutSwapBuffers(); ///自己寫
+}
+
+
+int main(int argc, char *argv[]) ///我們的 main函式 138行
+{
+    glutInit(&argc, argv); ///初始化 GLUT 140行
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); ///143行
+
+    glutCreateWindow("week10-5"); ///145行 建視窗
+
+    glutDisplayFunc(display); ///148行 設定要畫圖的display函式
+    glutIdleFunc(display); ///week10-2
+
+    glutMainLoop(); ///174行 主要迴圈
+}
